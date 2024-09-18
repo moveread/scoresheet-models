abae7e85dacf1de25239669949284042533a828f
m4rs <marcel@marcelclaramunt.cat>
2024-09-18 14:42:48 +0200

    cleaner drawing

--- py/src/scoresheet_models/drawing.py
+++ py/src/scoresheet_models/drawing.py
@@ -16,5 +16,8 @@ def draw(
   t = t * [w, h]
   cols = s[0]*np.array(model.col_positions) + t[0]
   rows = s[1]*np.array(model.row_positions) + t[1]
-  out = vc.draw.vlines(img, cols, thickness=thickness, color=color)
-  return vc.draw.hlines(out, rows, thickness=thickness, color=color)
+
+  hlines = np.array([[[t[0], y, s[0] + t[0], y]] for y in rows], dtype=np.int32)
+  vlines = np.array([[[x, t[1], x, s[1] + t[1]]] for x in cols], dtype=np.int32)
+  
+  return vc.draw.lines(img, np.concatenate([hlines, vlines]), thickness=thickness, color=color)
